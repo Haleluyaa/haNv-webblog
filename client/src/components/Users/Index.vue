324ed7660b55ac1f3826bc719c192882379fecd8
Haleluyaa Aki <haleluyaa.aki@gmail.com>
2019-04-21 22:55:49 +0700

    revise show info

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,12 +1,39 @@
 <template>
     <div>
         <h1>Get All Users</h1>
+        <h4>จำนวนผู้ใช้งาน {{users.length}}</h4>
+        <div v-for="user in users" v-bind:key="user.id">
+            <p>id : {{user.id}}</p>
+            <p>ชื่อ-นามสกุล :{{user.name}} - {{user.lastname}}</p>
+            <p>email : {{user.email}}</p>
+            <p>password : {{user.password}}</p>
+            <p><button v-on:click="navigateTo('/user/'+user.id)">ดูข้อมูลผู้ใช้</button></p>
+        </div>
     </div>
 </template>
-<script>
-    export default{
 
+<script>
+
+import UsersService from '@/services/UsersService'
+
+export default {
+    data () {
+        return{
+            users: []
+        }
+    },
+    async created (){
+        /*let results = (await UsersService.index()).data
+        console.log(results)*/
+        this.users = (await UsersService.index()).data
+    },
+    methods: {
+        navigateTo (route) {
+            this.$router.push(route)
+        }
     }
+}
+
 </script>
 <style scoped>
 
